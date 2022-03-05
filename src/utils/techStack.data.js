3ac916a2669ef5454b8e34fdca2def3192e15f15
gurjung <gurjung997@gmail.com>
2022-03-06 01:00:38 +0530

    chore: adds techstack data

---
+++ src/utils/techStack.data.js
@@ -0,0 +1,48 @@
+import { ImHtmlFive2 } from "react-icons/im";
+import { SiCss3 } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io";
+// import { SiCss3 } from "react-icons/si";
+// import { SiCss3 } from "react-icons/si";
+
+export const TECH_STACK_DATA = [
+  {
+    title: "HTML",
+    logo: <ImHtmlFive2 />,
+  },
+  {
+    title: "CSS",
+    logo: <SiCss3 />,
+  },
+  {
+    title: "JS",
+    logo: <IoLogoJavascript />,
+  },
+  {
+    title: "React",
+    logo: <ImHtmlFive2 />,
+  },
+  {
+    title: "Material UI",
+    logo: <ImHtmlFive2 />,
+  },
+  {
+    title: "Ant Design",
+    logo: <ImHtmlFive2 />,
+  },
+  {
+    title: "Tailwind CSS",
+    logo: <ImHtmlFive2 />,
+  },
+  {
+    title: "SCSS",
+    logo: <ImHtmlFive2 />,
+  },
+  {
+    title: "React Leaflet",
+    logo: <ImHtmlFive2 />,
+  },
+  {
+    title: "Chart.js",
+    logo: <ImHtmlFive2 />,
+  },
+];
