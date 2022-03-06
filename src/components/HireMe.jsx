5e6d8adecffe17c2338d63c31b225e05cbedfdcc
gurjung <gurjung997@gmail.com>
2022-03-06 23:16:45 +0530

    chore: adds email sending feature

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { AiFillLinkedin, AiFillGithub } from "../icons";
+
 export const HireMe = () => {
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+    emailjs
+      .sendForm(
+        "service_9n079dg",
+        "template_vppwp0s",
+        formRef.current,
+        "Gfu_INqQ9eCpWU25M"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setDone(false);
+        }
+      );
+  };
   return (
-    <div className="container flex items-center justify-between w-full px-8 mx-auto mt-64 md:px-14 lg:px-24">
+    <div className="container flex items-center justify-between w-full px-8 mx-auto mt-20 md:mt-48 md:px-14 lg:px-24">
       <section className="w-full">
         <h2 id="hire" className="secondary-title">
           Hire me
@@ -13,41 +38,49 @@ export const HireMe = () => {
 
         <div className="grid w-full gap-8 mt-24 lg:grid-cols-2 lg:gap-32">
           <div className="space-y-12">
-            <div>
-              <label className="block mb-6 text-xl font-bold text-white">
+            <form ref={formRef} onSubmit={handleSubmit}>
+              <label className="block mb-2 text-xl font-bold text-white">
                 Name
               </label>
-              <input className="w-full px-4 py-4 border border-input-border bg-input" />
-            </div>
-            <div>
-              <label className="block mb-6 text-xl font-bold text-white">
+              <input
+                type="text"
+                name="user_name"
+                className="w-full px-4 py-4 border border-input-border bg-input"
+              />
+              <label className="block mb-2 text-xl font-bold text-white">
+                Subject
+              </label>
+              <input
+                type="text"
+                name="user_subject"
+                className="w-full px-4 py-4 border border-input-border bg-input"
+              />
+              <label className="block mb-2 text-xl font-bold text-white">
                 Email
               </label>
               <input
                 type="email"
+                name="user_email"
                 className="w-full px-4 py-4 border border-input-border bg-input"
               />
-            </div>
-            <div>
-              <label className="block mb-6 text-xl font-bold text-white">
+              <label className="block mb-2 text-xl font-bold text-white">
                 Message
               </label>
               <textarea
-                type="email"
+                type="text"
+                name="message"
                 className="w-full h-56 px-4 py-4 border resize-none border-input-border bg-input"
               ></textarea>
-            </div>
-            <button className="px-6 py-2 font-bold text-white bg-theme">
-              Send it!
-            </button>
+              <button className="px-6 py-2 mb-2 font-bold text-white bg-theme">
+                Send it!
+              </button>
+            </form>
+            {done && <span>"Thank you contacting me..."</span>}
           </div>
 
           <div className="mt-12">
             <p className="text-secondary">12345678</p>
-            <a
-              href="mailto:email@mydomain.com"
-              className="block mt-3 underline text-secondary"
-            >
+            <a href="#/" className="block mt-3 underline text-secondary">
               email@mydomain.com
             </a>
 
@@ -65,3 +98,15 @@ export const HireMe = () => {
     </div>
   );
 };
+{
+  /* 
+<form ref={formRef} onSubmit={handleSubmit}>
+<input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
+<input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Subject" name="user_subject" />
+<input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Email" name="user_email" />
+<textarea style={{backgroundColor: darkMode && "#333"}} rows="5" placeholder="Message" name="message" />
+<button>Submit</button>
+{done && "Thank you..."}
+</form> 
+*/
+}
