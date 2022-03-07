aaffbad1e79b61040319d0027ee29de4d2e103a5
gurjung <gurjung997@gmail.com>
2022-03-08 00:46:54 +0530

    refactor: updates intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,20 +1,33 @@
 import React from "react";
-import { ViewIcon } from '../icons'
-
+import { ViewIcon } from "../icons";
+import Typed from "react-typed";
+import { TEXTS } from "../constants";
 export const Intro = () => {
   return (
     <>
-      <div className="flex flex-wrap justify-center max-w-xl mt-0 shadow-2xl lg:ml-20 md:justify-start md:my-20">
-        <h1 className="text-5xl font-bold text-center md:text-6xl lg:text-7xl md:text-left">
-          Building
-          <br /> beautiful web experiences
-        </h1>
-        <div className="flex justify-center w-full md:justify-start">
-          <button className="flex items-center px-8 py-4 mt-12 space-x-3 font-bold text-white bg-theme">
+      <div className="flex flex-wrap justify-center mx-auto mt-8 align-center lg:ml-20 md:my-20">
+        <Typed
+          strings={[
+            TEXTS.INTRO_DESC1,
+            TEXTS.INTRO_DESC2,
+          ]}
+          typeSpeed={100}
+          backSpeed={100}
+          loop
+          className="text-5xl font-bold text-center text-white md:text-6xl lg:text-7xl md:text-left bg-body"
+        ></Typed>
+        <div className="flex flex-col items-center justify-center w-full md:flex-row">
+          <button className="flex items-center px-8 py-4 mx-4 space-x-3 font-bold text-white rounded-lg md:mt-12 bg-theme">
             <div>
               <ViewIcon />
             </div>
-            <span>View my work.</span>
+            <a href="#projects">{TEXTS.VIEW_WORK}</a>
+          </button>
+          <button className="flex items-center px-8 py-4 mx-4 mt-4 space-x-3 font-bold text-white rounded-lg md:mt-12 bg-theme">
+            <div>
+              <ViewIcon />
+            </div>
+            <a href="#hire">{TEXTS.VIEW_CV}</a>
           </button>
         </div>
       </div>
