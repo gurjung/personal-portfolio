54a62e48d2c85a2bbc014e675402427c08252e37
gurjung <gurjung997@gmail.com>
2022-03-08 16:53:31 +0530

    feat: make navbar responsive

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,27 +1,69 @@
-import React from "react";
-import { MenuIcon } from "../icons";
+import React, { useState } from "react";
+import { MenuIcon, CgClose } from "../icons";
 import { TEXTS } from "../constants";
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav className="py-6 mb-8">
-      <div className="container flex items-center justify-between w-full px-8 mx-auto shadow-2xl md:px-14 lg:px-24">
-        <div className="text-lg font-bold">{TEXTS.NAME}</div>
-        <div className="items-center hidden space-x-12 md:flex">
-          <a href="#/" className="text-selected-text">
-            {TEXTS.URIS.HOME}
-          </a>
-          <a href="#tech">{TEXTS.URIS.TECH}</a>
-          <a href="#projects">{TEXTS.URIS.PROJECTS}</a>
-          <a href="#hire">
-            <button className="px-6 py-2 font-bold rounded-lg bg-theme">
-              {TEXTS.URIS.CONTACT}
-            </button>
-          </a>
-        </div>
-        <div className="md:hidden">
-          <MenuIcon />
+      <div className="flex items-center justify-between mr-2">
+        <section className="flex justify-between">
+          <span className="flex items-center ml-2 text-lg font-bold">
+            {TEXTS.NAME}
+          </span>
+          <div className="absolute right-0 items-center hidden mr-2 space-x-6 md:space-x-12 md:flex">
+            <a href="#/" className="text-lg text-selected-text md:text-2xl">
+              {TEXTS.URIS.HOME}
+            </a>
+            <a href="#tech" className="sm:text-lg md:text-2xl">
+              {TEXTS.URIS.TECH}
+            </a>
+            <a href="#projects" className="sm:text-lg md:text-2xl">
+              {TEXTS.URIS.PROJECTS}
+            </a>
+            <a href="#hire" className="sm:text-lg md:text-2xl">
+              <button className="px-6 py-2 font-bold rounded-lg bg-theme">
+                {TEXTS.URIS.CONTACT}
+              </button>
+            </a>
+          </div>
+        </section>
+        <div
+          className="flex items-center justify-end md:hidden"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <CgClose /> : <MenuIcon />}
         </div>
       </div>
+
+      {/* mobile menu */}
+      {open && (
+        <div className="mt-8 md:hidden">
+          <a
+            href="#/"
+            className="block px-4 py-2 mt-2 text-base text-center hover:bg-theme"
+          >
+            {TEXTS.URIS.HOME}
+          </a>
+          <a
+            href="#tech"
+            className="block px-4 py-2 mt-2 text-base text-center hover:bg-theme"
+          >
+            {TEXTS.URIS.TECH}
+          </a>
+          <a
+            href="#projects"
+            className="block px-4 py-2 mt-2 text-base text-center hover:bg-theme"
+          >
+            {TEXTS.URIS.PROJECTS}
+          </a>
+          <a
+            href="#hire"
+            className="block px-4 py-2 mt-2 text-base text-center hover:bg-theme"
+          >
+            {TEXTS.URIS.CONTACT}
+          </a>
+        </div>
+      )}
     </nav>
   );
 };
