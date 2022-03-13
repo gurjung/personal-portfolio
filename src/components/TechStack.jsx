5b1b08f1a8b0d7c62ba7389b8fc2c284355a429c
gurjung <gurjung997@gmail.com>
2022-03-13 14:52:25 +0530

    chore: improves styling

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -4,9 +4,9 @@ import { Card } from "../ui/Card";
 import { TEXTS } from "../constants";
 export const TechStack = () => {
   return (
-    <div className="container flex flex-col items-center justify-between w-full px-8 mx-auto mt-20 shadow-2xl md:mt-48 md:px-14 lg:px-24">
+    <div className="container flex flex-col items-center justify-between w-full px-8 mx-auto mt-20 shadow-2xl md:mt-44 md:px-14 lg:px-24">
       <p id="tech">{TEXTS.TECHSTACK}</p>
-      <div className="grid grid-cols-2 gap-4 mt-6 md:gap-12 sm:gap-8 sm:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-2 gap-4 py-4 mt-6 md:gap-12 sm:gap-8 sm:grid-cols-3 lg:grid-cols-4">
         {TECH_STACK_DATA.map((item) => (
           <Card>
             <>
